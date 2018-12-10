6077c48ba46b66fbc1201ca3be519d250b435f3c
An, Hyeong-woo <mytory@gmail.com>
2018-12-11 01:33:34 +0900

    13 싱글 페이지 만들기

---
+++ singular.php
@@ -0,0 +1,27 @@
+<?php
+get_header();
+
+the_post();
+?>
+
+    <article>
+        <header>
+            <h1><?php the_title(); ?></h1>
+        </header>
+
+        <?php if (!is_page()) { ?>
+            <aside>
+                <?php the_author(); ?>
+                | <?php the_date(); ?>
+            </aside>
+        <?php } ?>
+
+        <div>
+            <?php the_content(); ?>
+        </div>
+    </article>
+
+
+<?php
+
+get_footer();
