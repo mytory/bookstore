79f29732eb496e03826e5a9f1adf8c47cd8ba39b
An, Hyeong-woo <mytory@gmail.com>
2018-12-21 01:30:17 +0900

    18 책 정보 입력 항목 만들기 3-3 목차용 에디터 만들기

--- functions-meta-box.php
+++ functions-meta-box.php
@@ -1,6 +1,19 @@
 <?php
 add_action('add_meta_boxes_book', function () {
+
     add_meta_box('book-detail', '책 상세 정보', function () {
         include 'meta-box/book-detail.php';
     }, 'book');
+
 });
+
+
+add_action('edit_form_advanced', function () {
+
+    if (get_current_screen()->post_type === 'book') {
+        include 'meta-box/book-toc.php';
+    }
+
+});
+
+
---
+++ meta-box/book-toc.php
@@ -0,0 +1,14 @@
+<div class="postbox">
+
+    <h2>목차</h2>
+    <hr style="margin: 0">
+
+    <div class="inside">
+        <?php
+        wp_editor('', 'book_toc', [
+            'textarea_name' => 'meta[toc]',
+        ]);
+        ?>
+    </div>
+
+</div>
