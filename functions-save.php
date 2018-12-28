0f71440205402c54befc64350fc80908ae1dc699
An, Hyeong-woo <mytory@gmail.com>
2018-12-29 01:00:40 +0900

    21 책 정보 저장하기 2 post meta

--- functions-save.php
+++ functions-save.php
@@ -1,5 +1,8 @@
 <?php
 add_action('save_post_book', function () {
-    var_dump($_POST['meta']);
-    exit;
+
+    foreach ($_POST['meta'] as $k => $v) {
+        update_post_meta($_POST['ID'], $k, $v);
+    }
+
 });
