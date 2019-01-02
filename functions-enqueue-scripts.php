766f0c1c781fcd7460cc34ada5c33cff4c4adbdd
An, Hyeong-woo <mytory@gmail.com>
2019-01-02 23:41:18 +0900

    25 표지 정보 입력하기 2 - media 창 열기

--- functions-enqueue-scripts.php
+++ functions-enqueue-scripts.php
@@ -7,7 +7,7 @@ add_action('admin_enqueue_scripts', function () {
         wp_enqueue_script(
             'mbs_media',
             get_template_directory_uri() . '/js/book-edit-media.js',
-            [],
+            ['media-views'],
             '2018-12-30',
             true
         );
--- js/book-edit-media.js
+++ js/book-edit-media.js
@@ -1,1 +1,14 @@
-alert(1);
+document.querySelector('.js-open-book-cover-media').addEventListener('click', function () {
+
+    var media = wp.media({
+        title: '표지를 선택해 주세요',
+        library: {
+            type: 'image'
+        },
+        button: {
+            text: '넣기'
+        }
+    });
+    media.open();
+
+});
