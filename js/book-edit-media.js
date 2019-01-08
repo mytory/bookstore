aaa8e46021bd7ca766cd1245674f0b400b383d9b
An, Hyeong-woo <mytory@gmail.com>
2019-01-09 01:17:51 +0900

    29 표지 정보 입력하기 5 - 함수 정리

--- js/book-edit-media.js
+++ js/book-edit-media.js
@@ -7,43 +7,66 @@
         document.querySelector('.js-remove-book-cover').style.display = 'inline-block';
     }
 
-    var media = wp.media({
-        title: '표지를 선택해 주세요',
-        library: {
-            type: 'image'
-        },
-        button: {
-            text: '넣기'
-        }
-    });
-
-    document.querySelector('.js-open-book-cover-media').addEventListener('click', function () {
-        media.open();
-    });
-
-    media.on('select', function () {
-        var attachment = media.state()
-            .get('selection')
-            .first()
-            .toJSON();
-
-        renderCoverAndShowRemoveButton(attachment);
-
-        document.querySelector('[name="meta[cover_id]"]').value = attachment.id;
-
-    });
-
-    if (document.querySelector('[name="meta[cover_id]"]').value) {
-        wp.media.attachment(document.querySelector('[name="meta[cover_id]"]').value)
-            .fetch()
-            .then(renderCoverAndShowRemoveButton);
+    function initMediaObject() {
+        return wp.media({
+            title: '표지를 선택해 주세요',
+            library: {
+                type: 'image'
+            },
+            button: {
+                text: '넣기'
+            }
+        });
     }
 
-    document.querySelector('.js-remove-book-cover').addEventListener('click', function () {
-        document.querySelector('[name="meta[cover_id]"]').value = '';
-        document.querySelector('.js-book-cover-thumbnail').innerHTML = '';
-        this.style.display = 'none';
-    });
+    function bindOpeningMediaLibrary() {
+        document.querySelector('.js-open-book-cover-media').addEventListener('click', function () {
+            media.open();
+        });
+    }
 
+    function bindSelectCover() {
+        media.on('select', function () {
+            var attachment = media.state()
+                .get('selection')
+                .first()
+                .toJSON();
+
+            renderCoverAndShowRemoveButton(attachment);
+
+            document.querySelector('[name="meta[cover_id]"]').value = attachment.id;
+
+        });
+    }
+
+    function renderCoverAlreadyHas() {
+        if (document.querySelector('[name="meta[cover_id]"]').value) {
+            wp.media.attachment(document.querySelector('[name="meta[cover_id]"]').value)
+                .fetch()
+                .then(renderCoverAndShowRemoveButton);
+        }
+    }
+
+    function bindRemoveCover() {
+        document.querySelector('.js-remove-book-cover').addEventListener('click', function () {
+            document.querySelector('[name="meta[cover_id]"]').value = '';
+            document.querySelector('.js-book-cover-thumbnail').innerHTML = '';
+            this.style.display = 'none';
+        });
+
+    }
+
+
+
+
+    var media = initMediaObject();
+
+    bindOpeningMediaLibrary();
+
+    bindSelectCover();
+
+    bindRemoveCover();
+
+    renderCoverAlreadyHas();
 
 }());
