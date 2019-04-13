dbc5955fb07889f9e4d433389f4e93452ecf22e6
An, Hyeong-woo <mytory@gmail.com>
2019-04-13 18:21:58 +0900

    52강. 메뉴 기능 활용해 내비게이션 만들기 4

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php get_header() ?>
 
 
+<div class="wrapper">
 
-<ul>
-    <?php
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-            ?>
-            <li>
-                <a href="<?php the_permalink() ?>">
-                    <?php the_title(); ?>
-                </a>
-            </li>
-            <?php
-        }
-    } ?>
-</ul>
+    <ul>
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                ?>
+                <li>
+                    <a href="<?php the_permalink() ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </li>
+                <?php
+            }
+        } ?>
+    </ul>
 
+</div>
 
 <?php get_footer() ?>
