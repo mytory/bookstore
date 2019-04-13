4091859c8aa7469b8b3eb6dba11c1b6b16eea564
An, Hyeong-woo <mytory@gmail.com>
2019-04-13 18:00:24 +0900

    51 메뉴 기능 활용해 내비게이션 만들기 3

--- header.php
+++ header.php
@@ -1,20 +1,25 @@
 <html>
 <head>
     <?php wp_head() ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
 </head>
 <body>
 
 <div>
-    <?php if (function_exists('the_custom_logo')) {
-        the_custom_logo();
-    } ?>
+    <div class="wrapper">
+        <?php if (function_exists('the_custom_logo')) {
+            the_custom_logo();
+        } ?>
+    </div>
 
-    <nav>
-        <?php
-        if (has_nav_menu('main_nav')) {
-            wp_nav_menu(['theme_location' => 'main_nav']);
-        } else { ?>
-            <p>메인 내비게이션에 메뉴를 할당해 주세요.</p>
-        <?php } ?>
+    <nav class="nav-bg">
+        <div class="wrapper">
+            <?php
+            if (has_nav_menu('main_nav')) {
+                wp_nav_menu(['theme_location' => 'main_nav']);
+            } else { ?>
+                <p>메인 내비게이션에 메뉴를 할당해 주세요.</p>
+            <?php } ?>
+        </div>
     </nav>
 </div>
