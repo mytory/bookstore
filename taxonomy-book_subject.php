bbefb295842f0561520a14cf3a1119f012cf9458
An, Hyeong-woo <mytory@gmail.com>
2019-06-29 00:29:50 +0900

    70 주제, 필자 목록 만들고 설명 출력하기

---
+++ taxonomy-book_subject.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+
+<div class="wrapper">
+
+    <h1><?= get_the_archive_title() ?></h1>
+
+    <?php
+    $term_description = term_description();
+    if ($term_description) { ?>
+        <div class="description-box  margin-bottom">
+            <?= term_description() ?>
+        </div>
+    <?php } ?>
+
+    <ul class="basic-list">
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                ?>
+                <li class="basic-list-item">
+                    <a class="basic-list-link" href="<?php the_permalink() ?>">
+                        <h2 class="basic-list-title">
+                            <?php the_title(); ?>
+                        </h2>
+                        <div class="basic-list-meta">
+                            <?= get_post_meta(get_the_ID(), 'published_date', true) ?> 발행
+                        </div>
+                        <div class="basic-list-excerpt">
+                            <?php the_excerpt() ?>
+                        </div>
+                    </a>
+                </li>
+                <?php
+            }
+        } ?>
+    </ul>
+
+    <div class="text-center  padding">
+        <?= paginate_links() ?>
+    </div>
+
+</div>
+
+<?php get_footer() ?>
