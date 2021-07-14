f562fcb19f872725c119436cd97aa78a635b1914
An, Hyeong-woo <mytory@gmail.com>
2021-07-15 00:49:19 +0900

    85 메인 슬라이더 만들기 - 적용 1

--- functions-enqueue-scripts.php
+++ functions-enqueue-scripts.php
@@ -1,26 +1,41 @@
 <?php
-add_action('admin_enqueue_scripts', function () {
+add_action( 'admin_enqueue_scripts', function () {
 
-    $screen = get_current_screen();
+	$screen = get_current_screen();
 
-    if ($screen->id === 'book') {
-        wp_enqueue_script(
-            'mbs_media',
-            get_template_directory_uri() . '/js/book-edit-media.js',
-            ['media-views'],
-            '2018-12-30',
-            true
-        );
-    }
+	if ( $screen->id === 'book' ) {
+		wp_enqueue_script(
+			'mbs_media',
+			get_template_directory_uri() . '/js/book-edit-media.js',
+			[ 'media-views' ],
+			'2018-12-30',
+			true
+		);
+	}
 
+	wp_enqueue_style(
+		'mytory-bookstore-admin',
+		get_template_directory_uri() . '/admin.css',
+		array(),
+		1.1
+	);
+} );
 
+add_action( 'wp_enqueue_scripts', function () {
+	if ( is_home() ) {
+		wp_enqueue_script(
+			'swiperjs',
+			get_template_directory_uri() . '/js-lib/swiperjs/swiper-bundle.min.js',
+			[],
+			'6.7.5',
+			true
+		);
 
-
-    wp_enqueue_style(
-        'mytory-bookstore-admin',
-        get_template_directory_uri() . '/admin.css',
-        array(),
-        1.1
-    );
-
-});
+		wp_enqueue_style(
+			'swiperjs',
+			get_template_directory_uri() . '/js-lib/swiperjs/swiper-bundle.min.css',
+			array(),
+			'6.7.5'
+		);
+	}
+} );
