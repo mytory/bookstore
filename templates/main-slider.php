61aa93a06604ce3764f29d2038a1f090b7838cfc
An, Hyeong-woo <mytory@gmail.com>
2021-07-15 02:27:24 +0900

    88 메인 슬라이더 만들기 - 적용 4

--- templates/main-slider.php
+++ templates/main-slider.php
@@ -1,8 +1,10 @@
 <div class="swiper-container">
 	<div class="swiper-wrapper">
-		<div class="swiper-slide">Slide 1</div>
-		<div class="swiper-slide">Slide 2</div>
-		<div class="swiper-slide">Slide 3</div>
+		<div class="swiper-slide  main-slide-1">Slide 1</div>
+		<div class="swiper-slide  main-slide-2">Slide 2</div>
+		<div class="swiper-slide  main-slide-3">Slide 3</div>
+		<div class="swiper-slide  main-slide-4">Slide 3</div>
+		<div class="swiper-slide  main-slide-5">Slide 3</div>
 	</div>
 	<div class="swiper-pagination"></div>
 	<div class="swiper-button-prev"></div>
